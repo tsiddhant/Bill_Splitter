7812a9fba9162e120d6186b756b3dc071263a4e1
Siddhant Tripathi <tsiddhant456@gmail.com>
2019-08-31 10:18:14 +0530

    friends section mdified

---
+++ includes/friends_sidebar.php
@@ -0,0 +1,115 @@
+<div class="position-relative">
+    <div class="col-lg-3 d-flex ">
+        <div class="bg-light" id="sidebar-wrapper">
+            <div class="list-group list-group-flush">
+
+
+                <div class="container">
+                    <br><br>
+
+                    <br><br>
+
+                    <div class="well form-group">
+                        <h5>FRIEND REQUESTS
+                            <hr>
+                        </h5>
+                        <div class="form-group">
+                            <table class="table-borderless">
+                                <?php
+                                $query = "SELECT * FROM friend_request WHERE status = 'pending' AND user2_id = {$_SESSION['user_id']} ";
+                                $result_query = mysqli_query($connection, $query);
+                                while ($row = mysqli_fetch_assoc($result_query)) {
+                                    $friend = $row['user1_id'];
+                                    $query2 = "SELECT * FROM users WHERE user_id = {$friend}";
+                                    $result_query2 = mysqli_query($connection, $query2);
+                                    while ($row2 = mysqli_fetch_assoc($result_query2)) {
+                                        $id = $row2['user_id'];
+                                        $name = $row2['name'];
+                                        $user_name = $row2['username'];
+                                        echo "<tr><td>{$name}</td></tr>";
+                                        echo "<tr><td>{$user_name}</td>";
+                                        ?>
+                                        <td><a href="friends.php?request={$id}" class="col-sm-6">Accept</a></td></tr><tr><td>------------------</td></tr>
+
+                                <?php
+                                    }
+                                }
+                                ?>
+                            </table>
+                        </div>
+                    </div>
+
+                    <br><br>
+
+
+                    <?php
+                    $usernameErr = '';
+                    if (isset($_POST['request_friend'])) {
+
+                        $friend_id = $_POST['username'];
+                        $query = "SELECT * FROM users";
+                        $result_query = mysqli_query($connection, $query);
+                        while ($row = mysqli_fetch_assoc($result_query)) {
+                            if ($friend_id == $_SESSION['username']) {
+                                $usernameErr = '';
+                            } else if ($friend_id == $row['username']) {
+                                $usernameErr = "Username Exists!!";
+                            }
+                        }
+
+                        if (!empty($usernameErr)) {
+
+                            $query = "SELECT * FROM users WHERE username = '{$friend_id}' ";
+                            $select_user_profile_query = mysqli_query($connection, $query);
+                            while ($row = mysqli_fetch_array($select_user_profile_query)) {
+                                $user_id = $row['user_id'];
+                                $username = $row['username'];
+                                $user_name = $row['name'];
+                                $user_email = $row['email'];
+                                $user_number = $row['number'];
+                            }
+                            $to = $user_email;
+                            $subject = "Friend Request";
+                            $body = "You have a new friend request from : {$_SESSION['user_id']}.";
+
+                            if (mail($to, $subject, $body)) {
+                                echo ("Email send!");
+                            }
+                            $query = "INSERT INTO friend_request (`user1_id`, `user2_id`, `status`) ";
+                            $query .= "VALUES ('{$_SESSION['user_id']}', '{$user_id}', 'pending') ";
+                            $result_query = mysqli_query($connection, $query);
+
+                            if (!$result_query) {
+                                die("ERROR IN SENDING MAIL!!" . mysqli_error($connection));
+                            }
+                        }
+                        header("Location: friends.php");
+                    }
+
+                    ?>
+
+
+                    <div class="well form-group">
+                        <form action="" method="post">
+                            <h5 class="page-header">Send REQUESTS
+                                <hr>
+                            </h5>
+
+                            <div class="form-group">
+                                <input type="text" placeholder="Enter Username" class="form-control" name="username" required>
+                                <?php echo $usernameErr; ?>
+                            </div>
+                            <span>
+                                <div class="form-group col-sm-12">
+                                    <input class="btn btn-primary btn-block" type="submit" name="request_friend" value="SEND">
+                                </div>
+                            </span>
+                        </form>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</div>
