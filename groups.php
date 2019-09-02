35c39f2f19128002d60ef2d68239ce9e5b4bdbe7
jeevesh2001 <52119929+jeevesh2001@users.noreply.github.com>
2019-09-02 08:37:41 +0530

    Add group

--- groups.php
+++ groups.php
@@ -8,71 +8,108 @@
      
 
 
-<div class="table-responsive">
+<br>
+<button name="group" class="btn btn-primary" id="1">Add Group</button>
+
+<br><br>
+<form id="group" class="col-lg-4" action="" method="post">
+<div class="container jumbotron" id="new">
+    <label for="group">Type Group Name : </label>
+    <input type="text" class="form-control" placeholder="ENTER GROUP NAME" name="group_name">
+    <hr>
+    <input type="submit" class="btn btn-primary" name="add_group" value="SUBMIT">
+ 
+</div>
+</form>
+
+<?php
+if(isset($_POST['add_group'])){
+    $group_name     = $_POST['group_name'];
+    $admin_id       = $_SESSION['user_id'];
+    $admin_username = $_SESSION['username'];
+    $query = "INSERT INTO groups (`admin_id`, `admin_username`, `group_name`, `status`) ";
+    $query .= "VALUES ('{$admin_id}', '{$admin_username}', '{$group_name}', 'open') ";
+    $result = mysqli_query($connection,$query);
+    if(!$result){
+        die("ERROR".mysqli_error($connection));
+    }
+    header("Location: groups.php");
+}
+?>
+
+
+
+<style>
+#group {
+    display: none;
+}
+ 
+</style>
+<script>   
+$(document).ready(function(){
+  $("#1").click(function(){
+    $("form").toggle(400);
+  });
+});
+</script>
+
+
+<div class="table-responsive col-lg-6">
             <table class="table table-bordered table-hover table-warning">
                 <thead>
                     <tr>
-                        <th scope="col">User Id</th>
-                        <th scope="col">Username</th>
-                        <th scope="col">name</th>
-                        <th scope="col">number</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Remove</th>
+                        <th scope="col">Serial No.</th>
+                        <th scope="col">Group Name</th>
+                        <th scope="col">Admin Username</th>
+                        <th scope="col">Status</th>
+                        
+                        
                     </tr>
                 </thead>
                 <tbody class="table-hover">
 
-                
-
-
                     <?php
 
-
-
-
-                    $query = "SELECT users.* FROM groups JOIN friends ON groups.user_id = friends.user1_id JOIN users ON friends.user1_id = users.user_id";
-                   
-                   
-                    $select_members = mysqli_query($connection, $query);
-
-            
-                    while ($row = mysqli_fetch_assoc($select_members)) {
+                    $query = "SELECT * FROM groups WHERE members LIKE '%{$_SESSION['username']}%' OR admin_username LIKE '%{$_SESSION['username']}%' ";
+                    $result = mysqli_query($connection, $query);
+                    $i = 1;
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $group_name       = $row['group_name'];
+                        $admin_username   = $row['admin_username'];
+                        $status            = $row['status'];
                         
-                       
-                        $user_id             = $row['user_id'];
-                        $username            = $row['username'];
-                        $user_name           = $row['name'];
-                        $user_number         = $row['number'];
-                        $user_email          = $row['email'];
-
                         echo '<tr scope="row">';
-                        echo "<td>$user_id </td>";
-                        echo "<td>$username</td>";
-                        echo "<td>$user_name</td>";
-                        echo "<td>$user_number</td>";
-                        echo "<td>$user_email</td>";
-                        echo "<td><a href='groups.php?delete={$user_id}'>Delete</a></td>";
+                        echo "<td>$i</td>";
+                        echo "<td><a href='#'>$group_name</a></td>";
+                        echo "<td>$admin_username</td>";
+                        echo "<td>$status</td>";
+                        
+                        // echo "<td><a href='friends.php?delete={$user_id}'>Delete</a></td>";
                         echo "</tr>";
-
+                        $i++;
                     }
 
-                
-                
                     ?>
 
                 </tbody>
             </table>
         </div>
 
+        <?php
 
+        if (isset($_GET['delete'])) {
+            $the_user_id = ($_GET['delete']);
 
-      
+            $query = "DELETE FROM friendship WHERE user2_id = {$the_user_id} AND user1_id = {$_SESSION['user_id']}";
+            $delete_user_query = mysqli_query($connection, $query);
+            if (!$delete_user_query) {
+                die("ERROR!" . mysqli_error($connection));
+            }
+            header("Location: friends.php");
+        }
 
-
-
-
-
-
+        ?>
 
 
 <?php include "includes/admin_footer.php"; ?>
+
--- includes/admin_header.php
+++ includes/admin_header.php
@@ -16,7 +16,7 @@ if (!isset($_SESSION['username'])) {
     <meta name="author" content="">
 
     <title>Admin</title>
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <link href="css/simple-sidebar.css" rel="stylesheet">
