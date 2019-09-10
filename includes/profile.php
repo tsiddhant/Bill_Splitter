8304d9bbccb9ef461ff0ff532b290699264419a9
Siddhant Tripathi <tsiddhant456@gmail.com>
2019-09-10 19:49:56 +0530

    profile pic

--- includes/profile.php
+++ includes/profile.php
@@ -11,9 +11,28 @@ if (isset($_SESSION['username'])) {
         $user_name = $row['name'];
         $user_email = $row['email'];
         $user_number = $row['number'];
+        $picsource   = $row['picsource'];
     }
 }
 
+?>
+<?php 
+$friends_count=0;
+         $query = "SELECT p.* FROM friends f JOIN users p ON p.user_id = f.user1_id WHERE f.user2_id = {$_SESSION['user_id']} UNION SELECT p.* FROM friends f JOIN users p ON p.user_id = f.user2_id WHERE f.user1_id = {$_SESSION['user_id']}";
+         $select_friends = mysqli_query($connection, $query);
+         $friends_count = mysqli_num_rows($select_friends);
+$comments_count=0;
+        $query2 = "SELECT * FROM comments WHERE comment_author = '{$_SESSION['name']}' ";
+        $select_comments = mysqli_query($connection, $query2);
+        $comments_count = mysqli_num_rows($select_comments);
+$expenses_count=0;
+        $query3 = "SELECT * FROM expense WHERE paid_by = '{$_SESSION['username']}' ";
+        $select_expenses = mysqli_query($connection, $query3);
+        $expenses_count = mysqli_num_rows($select_expenses);
+$groups_count=0;
+        $query4 = "SELECT * FROM groups WHERE admin_username = '{$_SESSION['username']}' OR members LIKE '%{$_SESSION['username']}%' ";
+        $select_groups = mysqli_query($connection, $query4);
+        $groups_count = mysqli_num_rows($select_groups);
 ?>
 
 <script>
@@ -29,17 +48,22 @@ if (isset($_SESSION['username'])) {
 
 <?php
 
-  if(isset($_POST['submit']))
+  if(isset($_POST["submit"]))
  {   
+
+    if(file_exists($picsource)){
+        unlink($picsource);
+    }
+
+
     $filename = $_FILES["uploadfile"]["name"];
     $tempname = $_FILES["uploadfile"]["tmp_name"];
     $folder = "uploaded_images/".$filename;
     move_uploaded_file($tempname,$folder);
-    $query = "UPDATE SET ";
-
-
+ $query = "UPDATE users SET picsource = '{$folder}' WHERE user_id = '{$_SESSION['user_id']}' ";
+    $result_query = mysqli_query($connection,$query);
+  header("Location: profile.php");
  }
-unset($_POST);
 ?>
 
 <hr>
@@ -52,36 +76,34 @@ unset($_POST);
               
 
       <div class="text-center">
-        <img src="http://ssl.gstatic.com/accounts/ui/avatar_2x.png" class="avatar img-circle img-thumbnail" alt="avatar">
-        <h6>Upload a different photo...</h6>
-        <input type="file" class="text-center center-block file-upload" name = "uploadfile" value = "" >
-        <input type="submit" value="Upload Image" name="submit">
+        <img src="<?php  echo $picsource; ?>" class="avatar img-circle img-thumbnail" alt="No image!" style="border-radius:150px;">
+        <form action="" method="post" enctype="multipart/form-data">
+        <input type="file" class="text-center center-block file-upload" name = "uploadfile" value = "" required>
+        <button type="submit" value="Upload Image" name="submit">Upload Image</button>
+        </form>
       </div><br>
           
-          
-          <ul class="list-group">
-            <li class="list-group-item text-muted">Activity <i class="fa fa-dashboard fa-1x"></i></li>
-            <li class="list-group-item text-right"><span class="pull-left"><strong>Friends Added</strong></span> 125</li>
-            <li class="list-group-item text-right"><span class="pull-left"><strong>Groups Joined</strong></span> 13</li>
-            <li class="list-group-item text-right"><span class="pull-left"><strong>Comments Posted</strong></span> 37</li>
-            <li class="list-group-item text-right"><span class="pull-left"><strong>Payments Made</strong></span> 78</li>
-          </ul> 
+          <div class="container jumbotron-fluid">
+                <ul class="list-group">
+                    <li class="list-group-item text-muted">Activity <i class="fa fa-dashboard fa-1x"></i></li>
+                    <li class="list-group-item text-right"><span class="pull-left"><strong>Friends Added</strong></span><?php echo $friends_count;  ?></li>
+                    <li class="list-group-item text-right"><span class="pull-left"><strong>Groups Joined</strong></span><?php echo $groups_count;  ?></li>
+                    <li class="list-group-item text-right"><span class="pull-left"><strong>Comments Posted</strong></span><?php echo $comments_count;  ?></li>
+                    <li class="list-group-item text-right"><span class="pull-left"><strong>Payments Made</strong></span><?php echo $expenses_count;  ?></li>
+                </ul> 
                
-          <div class="panel panel-default">
-            <div class="panel-heading">Social Media</div>
-            <div class="panel-body">
-            	<i class="fa fa-facebook fa-2x"></i> <i class="fa fa-github fa-2x"></i> <i class="fa fa-twitter fa-2x"></i> <i class="fa fa-pinterest fa-2x"></i> <i class="fa fa-google-plus fa-2x"></i>
-            </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading offset-4">Social Media</div>
+                    <div class="panel-body offset-2">
+                        <a href="#"><i class="fa fa-facebook fa-2x"></i></a> <a href="#"><i class="fa fa-github fa-2x"></i></a> <a href="#"><i class="fa fa-twitter fa-2x"></i></a> <a href="#"><i class="fa fa-pinterest fa-2x"></i></a> <a href="#"><i class="fa fa-google-plus fa-2x"></i></a>
+                    </div>
+                </div>
           </div>
           
         </div><!--/col-3-->
     	<div class="col-sm-9">
-            <ul class="nav nav-tabs">
-                <li class="active"><a data-toggle="tab" href="#home">Home</a></li>
-            </ul>
-
-              
-          <div class="tab-content">
+              <br><br><br><br>
+          <div class="jumbotron col-lg-9 float-lg-right">
             <div class="tab-pane active" id="home">
                 <hr>
                         <form action="" method="post">
--- profile.php
+++ profile.php
@@ -21,7 +21,6 @@ if (!isset($_SESSION['username'])) {
 
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link href="css/simple-sidebar.css" rel="stylesheet">
 </head>
